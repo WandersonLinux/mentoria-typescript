3866fb369aea97b28ff19c46558844b2ba1df8a3
Wanderson Pereira <wandersonmetal123@hotmail.com>
2022-07-18 16:49:00 -0300

    Delete dist directory

--- dist/desafios/desafio1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
-const employee = {
-    code: 10,
-    name: 'Jhon'
-};
-//employee.code = 10;
-//employee.name = "John";
